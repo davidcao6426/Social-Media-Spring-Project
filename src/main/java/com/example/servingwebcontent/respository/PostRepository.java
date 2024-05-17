968343a30578c3bf44fb385c62fce2ce0f09496e
davidcao <davidcao6426@gmail.com>
2024-05-17 13:32:02 +0800

    add Post procedure and spring procedure connect

---
+++ src/main/java/com/example/servingwebcontent/respository/PostRepository.java
@@ -0,0 +1,19 @@
+package com.example.servingwebcontent.respository;
+
+import com.example.servingwebcontent.entity.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PostRepository extends CrudRepository<User, Integer> {
+    @Procedure("addPost")
+    void addPost(String p_postId, String p_userId, String p_content);
+    @Procedure("editPost")
+    void editPost(String p_postId, String p_content);
+    @Procedure("deletePost")
+    void editPost(String p_postId);
+    @Query(value = "CALL findAllPost", nativeQuery = true)
+    List<User> findUserById(String p_userId);
+}

8d0f5bad2eb44b36bfb4d42d63cb1dcea306e4aa
davidcao <davidcao6426@gmail.com>
2024-06-01 00:50:45 +0800

    find all user

--- src/main/java/com/example/servingwebcontent/respository/UserRepository.java
+++ src/main/java/com/example/servingwebcontent/respository/UserRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
@@ -13,4 +15,7 @@ public interface UserRepository extends CrudRepository<User, Integer> {
     void addUser(String p_userId, String p_username, String p_email, String p_password, String p_coverImage, String p_biography);
     @Query(value = "CALL findUserById(:p_userId)", nativeQuery = true)
     User findUserById(String p_userId);
+    @Query(value = "CALL findAllUser", nativeQuery = true)
+    List<User> findAllUser();
+
 }
--- src/main/java/com/example/servingwebcontent/service/UserServiceImpl.java
+++ src/main/java/com/example/servingwebcontent/service/UserServiceImpl.java
@@ -49,7 +49,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> findAllUsers() {
-        return List.of();
+        return userRepository.findAllUser();
     }
 
     @Override

61a83a687ff655c9d9debddf08f92e6280add5b8
davidcao <davidcao6426@gmail.com>
2024-06-16 01:54:27 +0800

    disable csrf

--- src/main/java/com/example/servingwebcontent/config/SecurityConfig.java
+++ src/main/java/com/example/servingwebcontent/config/SecurityConfig.java
@@ -2,10 +2,14 @@ package com.example.servingwebcontent.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -13,13 +17,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        String[] allowedPaths = { "/", "/user", "/api/**" };
         http
-                .csrf(Customizer.withDefaults())
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll()//authenticated()
-                )
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults());
+                .csrf(AbstractHttpConfigurer::disable);
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers(allowedPaths).permitAll()
+//                        .requestMatchers(antMatcher(HttpMethod.POST, "/api/**")).permitAll()
+////                        .requestMatchers(antMatcher(HttpMethod.POST, "/api/user/**")).hasRole("USER")//.hasRole("ADMIN")
+//                        .anyRequest().permitAll()//authenticated()
+//                );
         return http.build();
     }
 
